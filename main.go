a2646adc2f6f8f4a15038768898c5aaea7a6f3e7
Joshua Kwan <joshk@arbor.io>
2016-10-25 11:50:15 -0400

    Actually use only the standard library.
    It's part of our advertising after all.. this puts us past 35 SLOC
    though. :)

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
 	"text/template"
-
-	log "github.com/Sirupsen/logrus"
 )
 
 var (
@@ -19,17 +18,22 @@ func main() {
 	flag.Parse()
 
 	if *source == "" || *data == "" {
-		log.Fatal("please pass a template and data")
+		fatal("please pass a template and data")
 	}
 
 	var deserialized map[string]interface{}
 	if fp, err := os.Open(*data); err != nil {
-		log.WithField("err", err).Fatal("failed to open data file")
+		fatal(fmt.Sprintf("failed to open data file: %s", err))
 	} else if err = json.NewDecoder(fp).Decode(&deserialized); err != nil {
-		log.WithField("err", err).Fatal("failed to deserialize data file")
+		fatal(fmt.Sprintf("failed to deserialize data file: %s", err))
 	} else if t, err := template.New("").ParseFiles(*source); err != nil {
-		log.WithField("err", err).Fatal("failed to parse template")
+		fatal(fmt.Sprintf("failed to parse template: %s", err))
 	} else if err = t.ExecuteTemplate(os.Stdout, filepath.Base(*source), deserialized); err != nil {
-		log.WithField("err", err).Fatal("failed to render template")
+		fatal(fmt.Sprintf("failed to render template: %s", err))
 	}
+}
+
+func fatal(msg string) {
+	fmt.Fprintf(os.Stderr, "FATAL: %s\n", msg)
+	os.Exit(1)
 }
